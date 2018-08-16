b435297feb67479dc9db4ce8be01f81f4aa3974c
Zjiezi <597929460@qq.com>
2018-08-16 14:54:22 +0800

    *

--- src/main/java/com/bluewhite/portal/common/filter/WebAppConfig.java
+++ src/main/java/com/bluewhite/portal/common/filter/WebAppConfig.java
@@ -1,7 +1,9 @@
 package com.bluewhite.portal.common.filter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -11,16 +13,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebAppConfig implements WebMvcConfigurer  {  
-	  
+	
+    @Value("${web.upload-path}")
+    private String path; 
 	
     @Override  
     public void addInterceptors(InterceptorRegistry registry) {  
         //注册自定义拦截器，添加拦截路径和排除拦截路径  
         registry.addInterceptor(new InterceptorConfig())
         .addPathPatterns("/**")
-        .excludePathPatterns("/backLogin","/userLogin","/","/static/**","/upload/**","/menusToUrl");
+        .excludePathPatterns("/backLogin","/userLogin","/","/static/**","/menusToUrl",path);
     }
     
-	
+//    @Override  
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {  
+//    	registry.addResourceHandler("/upload/img/**").addResourceLocations("file:path");
+//    }
 
 }
