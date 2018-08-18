e31661981163f71191cfbf5fde953f6093a8e2d2
Zjiezi <597929460@qq.com>
2018-08-18 13:49:18 +0800

    *

--- src/main/java/com/bluewhite/portal/common/filter/WebAppConfig.java
+++ src/main/java/com/bluewhite/portal/common/filter/WebAppConfig.java
@@ -1,6 +1,5 @@
 package com.bluewhite.portal.common.filter;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -25,6 +24,10 @@ public class WebAppConfig implements WebMvcConfigurer  {
     
     @Override  
     public void addResourceHandlers(ResourceHandlerRegistry registry) {  
+    	/*
+         * 说明：增加虚拟路径(经过测试：在此处配置的虚拟路径，用springboot内置的tomcat时有效，
+         * 用外部的tomcat也有效;所以用到外部的tomcat时不需在tomcat/config下的相应文件配置虚拟路径了,阿里云linux也没问题)
+         */
     	registry.addResourceHandler("/upload/img/**").addResourceLocations("file:D:/upload/img/");
     }
 
