dcd035a4e4cbf6dad83c106da25eb0ef7d30c27e
Zjiezi <597929460@qq.com>
2018-09-07 15:45:27 +0800

    *

--- src/main/java/com/bluewhite/portal/system/user/action/UserAction.java
+++ src/main/java/com/bluewhite/portal/system/user/action/UserAction.java
@@ -1,17 +1,22 @@
 package com.bluewhite.portal.system.user.action;
 
+import java.text.SimpleDateFormat;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 
 import com.bluewhite.portal.common.ClearCascadeJSON;
+import com.bluewhite.portal.common.DateTimePattern;
 import com.bluewhite.portal.common.entity.CommonResponse;
 import com.bluewhite.portal.common.entity.PageParameter;
-import com.bluewhite.portal.file.entity.Files;
-import com.bluewhite.portal.product.entity.Product;
 import com.bluewhite.portal.system.user.entity.UserMessage;
 import com.bluewhite.portal.system.user.service.UserMessageService;
 
@@ -43,6 +48,17 @@ public class UserAction {
 		cr.setData(clearCascadeJSON.format(userMessageService.findPages(userMessage,page)).toJSON());
 		cr.setMessage("查找成功");
 		return cr;
+	}
+	
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateTimeFormat = new SimpleDateFormat(
+				DateTimePattern.DATEHMS.getPattern());
+		binder.registerCustomEditor(java.util.Date.class, null,
+				new CustomDateEditor(dateTimeFormat, true));
+		binder.registerCustomEditor(byte[].class,
+				new ByteArrayMultipartFileEditor());
 	}
 
 }
